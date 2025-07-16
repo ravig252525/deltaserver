e24e240416a2a69ef03c7a9c7ab5dd480ddd757b
ravig252525 <44760145+ravig252525@users.noreply.github.com>
2025-07-16 18:40:25 +0530

    Update ApplicationConfig.java

--- src/main/java/com/delta/server/delta/config/ApplicationConfig.java
+++ src/main/java/com/delta/server/delta/config/ApplicationConfig.java
@@ -30,6 +30,13 @@ import java.net.URI;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import io.r2dbc.spi.ConnectionFactory;
 @Configuration
 public class ApplicationConfig {
     @Bean
@@ -142,5 +149,16 @@ public class ApplicationConfig {
         return template;
     }
 
+
+
+     @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
 
 }

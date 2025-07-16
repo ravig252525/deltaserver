420970dd8371ca97ab9e04e01a80ce791fa84908
ravig252525 <44760145+ravig252525@users.noreply.github.com>
2025-07-16 18:32:41 +0530

    Update Watchlist.java

--- src/main/java/com/delta/server/delta/model/Watchlist.java
+++ src/main/java/com/delta/server/delta/model/Watchlist.java
@@ -3,11 +3,12 @@ package com.delta.server.delta.model;
 import lombok.Data;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.relational.core.mapping.Column;
-
+import org.springframework.data.annotation.Id;
 @Data
 @Table("watchlist")
 public class Watchlist {
-
+     @Id
+    private Long id;
     private String symbol;
     @Column("product_id")
     private Long productId;

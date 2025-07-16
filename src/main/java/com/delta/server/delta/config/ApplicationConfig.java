2dd6ed71461164635efccce9a39ab3ba724cdddb
ravig252525 <44760145+ravig252525@users.noreply.github.com>
2025-07-16 17:42:05 +0530

    Update ApplicationConfig.java

--- src/main/java/com/delta/server/delta/config/ApplicationConfig.java
+++ src/main/java/com/delta/server/delta/config/ApplicationConfig.java
@@ -1,12 +1,12 @@
 package com.delta.server.delta.config;
 
+
 import com.delta.server.delta.model.Candle;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 
 import io.r2dbc.spi.ConnectionFactory;
 import okhttp3.ConnectionPool;
@@ -15,15 +15,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.http.MediaType;
-import org.springframework.r2dbc.core.DatabaseClient;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.URI;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 

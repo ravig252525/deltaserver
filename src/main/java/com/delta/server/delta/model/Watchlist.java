b61a6e383b960b0425fdd704cb1f43bf1a3086c6
ravig252525 <44760145+ravig252525@users.noreply.github.com>
2025-07-18 16:45:51 +0530

    Add files via upload

---
+++ src/main/java/com/delta/server/delta/model/Watchlist.java
@@ -0,0 +1,20 @@
+package com.delta.server.delta.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table("watchlist")
+public class Watchlist {
+
+    @Id
+    private Long id;
+    private String symbol;
+    @Column("productid")
+    private Long productId;
+    private String description;
+    @Column("userid")
+    private String userId;
+}
